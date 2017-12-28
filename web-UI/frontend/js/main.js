622c92430e9555922ff3d7e8c8660a7a783bf905
Gabriele Serra <gabriele_serra@hotmail.it>
2017-12-28 01:21:30 +0100

    Ajax integration and server side code
    Added server side code (PHP) plus ajax integration between home page and server code

--- web-UI/frontend/js/main.js
+++ web-UI/frontend/js/main.js
@@ -4,10 +4,126 @@
 // -----------------------
 
 $(document).ready(function(){
-    toggleTooltip();
+    redirect_if_logged();
+    toggle_tooltip();
+    $("#form-login").vindicate("init");
+    $("#form-signup").vindicate("init");
 });
 
-// toggle on boostrap tooltip
-function toggleTooltip() {
+// -------------------------------------
+// REDIRECT USER FUNCTION
+// -------------------------------------
+
+// Redirect user to dashboard if already logged i
+function redirect_if_logged() {
+    ajax_req(
+        "php/redirect.php", 
+        "",     
+        redirect_succ, 
+        redirect_err
+    );
+}
+
+// Action done in case of success
+function redirect_succ(reply) {
+    if (reply.error == false)
+        window.location.replace("http://localhost/XDR/web-UI/dashboard/");
+}
+
+// Action done in case of failure
+function redirect_err() {
+    // maybe something in future...
+}
+
+// -------------------------------------
+// SUBMIT LOGIN FORM SUCCESS
+// -------------------------------------
+
+// Submit validation form login
+function submit_login() {
+    $("#form-login").vindicate("validate");
+
+    ajax_req(
+        "php/login.php", 
+        $("#form-login").serialize(),     
+        login_succ, 
+        login_err
+    );
+}
+
+// Action done in case of success
+function login_succ(reply) {
+    if (reply.error == false) {
+        window.location.replace("http://localhost/XDR/web-UI/dashboard/");
+    } else {
+        $(".login-error-title").html("Error: ");
+        $(".login-error-text").html(reply.message);
+        show_alert(".alert-login");
+    }
+}
+
+// Action done in case of failure
+function login_err() {
+    $(".login-error-title").html("Error: ");
+    $(".login-error-text").html("server unreachable.");
+    show_alert(".alert-login");
+}
+
+// -------------------------------------
+// SUBMIT SIGNUP FORM SUCCESS
+// -------------------------------------
+
+// Submit validation form sign up
+function submit_signup() {
+    $("#form-signup").vindicate("validate");
+    
+    ajax_req(
+        "php/signup.php", 
+        $("#form-signup").serialize(),     
+        signup_succ, 
+        signup_err
+    );
+}
+
+// Action done in case of success
+function signup_succ(reply) {
+    if (reply.error == false) {
+        alert(reply.message);
+    } else {
+        $(".signup-error-title").html("Error: ");
+        $(".signup-error-text").html(reply.message);
+        show_alert(".alert-signup");
+    }
+}
+
+// Action done in case of failure
+function signup_err() {
+    $(".signup-error-title").html("Error: ");
+    $(".signup-error-text").html("server unreachable.");
+    show_alert(".alert-signup");
+}
+
+// -------------------------------------
+// UTILITY
+// -------------------------------------
+
+function ajax_req(dest, info, succ, err) {
+    $.ajax({
+        type: "POST",
+        url: dest,
+        data: info,
+        dataType: "json",
+        success: succ,
+        error: err
+    });
+}
+
+// Toggle on boostrap tooltip
+function toggle_tooltip() {
     $('[data-toggle="tooltip"]').tooltip(); 
+}
+
+function show_alert(htmlclass) {
+    $(htmlclass).removeClass("d-none");
+    $(htmlclass).addClass("show");
 }
---
+++ web-UI/frontend/php/session.php
@@ -0,0 +1,56 @@
+<?php
+// -------------------------------------
+// SESSION.PHP
+// contains php script to set/reset
+// super global $_SESSION array
+// -------------------------------------
+
+// Set $_SESSION array with logged user data
+function set_session_array($username, $name, $surname, $bio, $avatar, $cover) {
+    $_SESSION['username'] = $username;
+    $_SESSION['name'] = $name;
+    $_SESSION['surname'] = $surname;
+    $_SESSION['bio'] = $bio;
+    $_SESSION['avatar'] = $avatar;
+    $_SESSION['cover'] = $cover;
+}
+
+// Check if user is logged in
+function is_user_logged() {
+    if(isset($_SESSION['username']))
+        return true;
+    else
+        return false;
+}
+
+// Return username of logged user
+function get_user_username() {
+    return $_SESSION['username'];
+}
+
+// Return name of logged user
+function get_user_name() {
+    return $_SESSION['name'];
+}
+
+// Return surname of logged user
+function get_user_surname() {
+    return $_SESSION['surname'];
+}
+
+// Return bio of logged user
+function get_user_bio() {
+    return $_SESSION['bio'];
+}
+
+// Return avatar of logged user
+function get_user_avatar() {
+    return $_SESSION['avatar'];
+}
+
+// Return cover of logged user
+function get_user_cover() {
+    return $_SESSION['cover'];
+}
+
+?>
