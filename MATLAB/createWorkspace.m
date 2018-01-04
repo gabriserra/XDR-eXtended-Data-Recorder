31fd0841288ff3c20676b4a51812b567b3e1f56e
Silvio Bacci <ciabbi94@live.it>
2018-01-04 10:27:22 +0100

    Aggiornamento script MATLAB
    Codice ottimizzato per leggere velocemente i log in modo automatico

--- MATLAB/createWorkspace.m
+++ MATLAB/createWorkspace.m
@@ -1,16 +1,22 @@
 function createWorkspace()
-    files = dir('../server/log/URTO/*');
-    
-    for file = files'
-        if(contains(file.name,'log') == 1)
-            filename = erase(strrep(file.name, '-','_'),'.csv');
-            cmd = [filename '=readFile(strcat(''../server/log/URTO/'',file.name));'];
-            eval(cmd);
-            
-            if ~exist('dataset.mat','file')
-                save('dataset.mat',filename);
-            else
-                save('dataset.mat',filename,'-append');
+    path = '../server/log/';
+    folder = dir(strcat(path,'*'));
+    for sub_folder = folder'
+        if(~strcmp(sub_folder.name,'..')==1)
+            sub_folder_path = strcat(path,sub_folder.name,'/');
+            files = dir(strcat(sub_folder_path,'*'));
+            for file = files'
+                if(contains(file.name,'log') == 1)
+                    filename = erase(strrep(file.name, '-','_'),'.csv');
+                    cmd = [filename '=readFile(strcat(sub_folder_path,file.name));'];
+                    eval(cmd);
+
+                    if ~exist('dataset.mat','file')
+                        save('dataset.mat',filename);
+                    else
+                        save('dataset.mat',filename,'-append');
+                    end
+                end
             end
         end
     end
--- MATLAB/readFile.m
+++ MATLAB/readFile.m
@@ -27,5 +27,17 @@ gy_x = data{:, 6};
 gy_y = data{:, 7};
 gy_z = data{:, 8};
 
+index = find(timestamp > 10000);
+
+timestamp = removerows(timestamp,'ind',index);
+acc_x = removerows(acc_x,'ind',index);
+acc_y = removerows(acc_y,'ind',index);
+acc_z = removerows(acc_z,'ind',index);
+gy_x = removerows(gy_x,'ind',index);
+gy_y = removerows(gy_y,'ind',index);
+gy_z = removerows(gy_z,'ind',index);
+
 dim = min([size(timestamp,1), size(acc_x,1), size(acc_y,1), size(acc_z,1), size(v,1), size(gy_x,1), size(gy_y,1), size(gy_z,1)]);
 var = [timestamp(1:dim), acc_x(1:dim), acc_y(1:dim), acc_z(1:dim), v(1:dim), gy_x(1:dim), gy_y(1:dim), gy_z(1:dim)];
+[index, ~] = find(isnan(var));
+var = removerows(var,'ind',index);
