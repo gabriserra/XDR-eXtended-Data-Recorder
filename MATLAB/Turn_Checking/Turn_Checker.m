967d05319211dc6e8027b0d04b6a552b47fe0116
balde93 <34909863+balde93@users.noreply.github.com>
2018-01-05 10:01:11 +0100

    olo
    olo

--- MATLAB/Turn_Checking/Turn_Checker.m
+++ MATLAB/Turn_Checking/Turn_Checker.m
@@ -1,4 +1,4 @@
-function [a]=Turn_Checker(accx,accy,giroz)
+function [data]=Turn_Checker(accx,accy,giroz)
       if(size(accx,1) == 1)
         accx=accx';
       end
@@ -9,22 +9,40 @@ function [a]=Turn_Checker(accx,accy,giroz)
         accx=accx';
       end
     
-      turns = find(abs(giroz) > 10)
+      turns = find(giroz > 10);
       i=1;
       j=1;
-      next=0;
-     while (j<2)
-          end_turn = find(giroz(turns(i,1):size(giroz,1),1)< 0) -1;
-          next = turns(i,1)+end_turn(1,1);
-          %ripartire dal next alla prossima iterazioni (cercare il valore in turns subito maggiore di end_turn+turns)
-          data(j,1) = turns(i,1);                                             %starting point
-          data(j,2) = end_turn(1,1)      ;                                     %time interval   
-        %  data(j,3) = mean(accx(turns(i,1): end_turn(1,1)-turns(i,1)));
-        %  data(j,4) = mean(accy(turns(i,1): end_turn(1,1)-turns(i,1)));
-        %  data(j,5) = var(accy(turns(i,1): end_turn(1,1)-turns(i,1)));
-       %  turns = removerows(turns,'ind',index)
-        j=j+1;
+      next_index=turns(1,1);
+
+     while (j<  4)
+          end_turn = find(giroz(next_index:size(giroz,1),1)< 0) -1;
+          end_turn =end_turn(1,1);
+          next = next_index+end_turn(1,1);
+          
+          
+          data(j,1) = next_index;                                             %starting point
+          data(j,2) = end_turn(1,1);                                          %interval
+          data(j,3) = mean(abs(accx(next_index:next,1)));       
+          data(j,4) = mean(abs(accy(next_index:next,1)));       
+          data(j,5) = var(abs(accy(next_index:next,1)));  
+          
+          next_index = find(turns > next);
+          if(isempty(next_index))
+               break;
+          end
+          next_index = next_index(1,1);
+          next_index = turns(next_index,1);                                               %punto di partenza nuovo
+          
+          
+          turns=  turns(next_index:size(turns,1),1)     ; 
+         
+          
+                                                 %time interval   
         
+       %  turns = removerows(turns,'ind',index)
+          j=j+1;
+          
+     
         %rimuovi tutte gli elementi in turns compresi tra turns(i,1) e il
         %risultato di find(0)
      end
