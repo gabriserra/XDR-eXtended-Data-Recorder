6d8db5336fdc2c07d03cf48f651b2d40aaec43d5
Silvio Bacci <ciabbi94@live.it>
2018-01-07 19:06:04 +0100

    Server perfezionato
    Il server può essere gestito da tastiera adesso.

--- server/server.c
+++ server/server.c
@@ -1,78 +1,202 @@
 //------------------------------------------------------------------------------
-// SERVER: 	Create server to receive data from the car
-//------------------------------------------------------------------------------
-
-//------------------------------------------------------------------------------
+// SERVER: 	Server to receive data from the car
+//
 // COMPILE COMMAND: clear && gcc server.c udp.c -o server && ./server
 //------------------------------------------------------------------------------
 
-#include <arpa/inet.h>
-#include <netinet/in.h>
-#include <stdio.h>
-#include <string.h>
-#include <sys/socket.h>
-#include <time.h>
+#include 	<arpa/inet.h>
+#include 	<errno.h>
+#include 	<inttypes.h>
+#include 	<netinet/in.h>
+#include 	<stdio.h>
+#include 	<string.h>
+#include 	<sys/select.h>
+#include 	<sys/socket.h>
+#include 	<time.h>
+#include 	"udp.h"
 
-#include "udp.h"
+//------------------------------------------------------------------------------
+// GLOBAL CONSTANTS
+//------------------------------------------------------------------------------
+
+#define		KEYBOARD		0
+#define 	SERVER_PORT		8000
+#define		IP_SIZE			16
+#define		TRUE			1
+#define		FALSE			0
+
+//------------------------------------------------------------------------------
+// GLOBAL STRUCTURES
+//------------------------------------------------------------------------------
 
 typedef struct tm* tm_t;
 
-sensor_data_g_t all_d;
+//------------------------------------------------------------------------------
+// GLOBAL VARIABLES
+//------------------------------------------------------------------------------
 
-int 	main() {
-int 	recvBytes;
-char 	filename[34];
-char* 	IP = 0;
+int 	sockt_UDP, fdmax = 0, i, CAR_PORT, STARTED = TRUE, CONN_CREATED = FALSE, counter;
+char 	buf[1024], filename[34], carIP[IP_SIZE], myIP[IP_SIZE], *IP = 0;
+ssize_t recvBytes;
 time_t 	rawtime;
 tm_t	tm;
-FILE*	fp = fopen ("", "r");
-address	carAddress;
+FILE*	fp;
+fd_set 	master, read_fds;
+sensor_data_g_t all_d;
+
+//------------------------------------------------------------------------------
+// CLEAR BUFFER: Clear stdin
+//------------------------------------------------------------------------------
+
+void clearBuffer() {
+	int c;
+	while((c = getchar())!='\n' && c != '\0') {}
+}
+
+//------------------------------------------------------------------------------
+// END: Check if user inserted "END" in the terminal
+//------------------------------------------------------------------------------
+
+int end() {
+	fgets(buf,sizeof(buf),stdin);
+	buf[(int)strlen(buf)-1]='\0';
 	
-		if (UDPrecv_init() < 0) {
-			printf("ERROR IN UDP RECV INIT\n");
+	if(strcmp(buf,"END") == 0)
+		return 1;
+	return 0;
+}
+
+//------------------------------------------------------------------------------
+// CREATE NEW FILE: Create new file to save data from car
+//------------------------------------------------------------------------------
+
+void createNewFile() {
+	printf("\nNEW CONNECTION ESTABLISHED WITH CAR (PORT: %d) (IP: %s)\n", CAR_PORT, carIP);
+	fflush(stdout);
+	counter = 0;
+	time(&rawtime);
+	tm = localtime(&rawtime);
+	sprintf(filename, "./log/log_%04d-%02d-%02d_%02d-%02d-%02d.csv", tm->tm_year+1900, tm->tm_mon+1, tm->tm_mday, tm->tm_hour, tm->tm_min, tm->tm_sec);
+	if (fp != NULL)
+		fclose(fp);
+	fp = fopen(filename,"a");
+}
+
+//------------------------------------------------------------------------------
+// RECV FROM CAR: Manages the communication with the car
+//------------------------------------------------------------------------------
+
+int recvFromCar() {
+	recvBytes = UDPrecv(&all_d);
+
+	if (recvBytes <= 0) {
+		printf("ERROR IN UDP RECV\n");
+		fflush(stdout);
+		return -1;
+	}
+	
+	switch(recvBytes) {
+		case sizeof(all_d.m):
+			CAR_PORT = all_d.m;
+			break;
+		case IP_SIZE:
+			IP = (char *) &all_d;
+			memcpy(carIP, IP, strlen(IP)+1);
+			createCarAddress(carIP, CAR_PORT);
+			UDPsend(myIP);
+			createNewFile();
+			CONN_CREATED = TRUE;
+			break;
+		case sizeof(sensor_data_g_t):
+			if (counter++ == 0) {
+				printf("START RECEIVING DATA ");
+				fflush(stdout);
+			}
+			else if (counter++ % 100 == 0){
+				printf(".");
+				fflush(stdout);
+			}
+			fprintf(fp,"%" PRIu32 "\t %" PRIu32 "\t %f\t %f\t %f\t\n", all_d.m, all_d.seq_num, all_d.ax, all_d.ay, all_d.gz);
+			break;
+		default:
+			printf("ERROR\n");
+			fflush(stdout);
+			break;
+	}
+	
+	return 0;
+}
+
+//------------------------------------------------------------------------------
+// MAIN: Server body
+//------------------------------------------------------------------------------
+
+int main() {
+	fp = fopen ("", "r");
+	
+	if ((sockt_UDP = UDPrecv_init(SERVER_PORT)) < 0) {
+		printf("ERROR IN UDP RECV INIT\n");
+		fflush(stdout);
+		if (fp != NULL)
+			fclose(fp);
+		return 0;
+	}
+	
+	IP = getMyIP();
+	memcpy(myIP, IP, strlen(IP)+1);
+	printf("START SERVER (PORT: %d) (IP: %s)\n",SERVER_PORT, myIP);
+	fflush(stdout);
+
+	FD_ZERO(&master);
+	FD_ZERO(&read_fds);
+	FD_SET(KEYBOARD, &master);
+	fdmax = KEYBOARD;
+	FD_SET(sockt_UDP, &master);
+	if (fdmax < sockt_UDP)
+		fdmax = sockt_UDP;
+
+	while(1) {
+		read_fds = master;
+		if(select(fdmax+1, &read_fds, NULL, NULL, NULL) < 0) {
+			printf("ERROR IN UDP SELECT\n");
+			fflush(stdout);
 			if (fp != NULL)
 				fclose(fp);
 			return 0;
 		}
-	
-		while(1) {
-			recvBytes = UDPrecv(&all_d, &carAddress);
-			
-			if (recvBytes <= 0) {
-				printf("ERROR IN UDP RECV\n");
-				if (fp != NULL)
-					fclose(fp);
-				return 0;
-			}
-			
-			if (recvBytes == 4) {
-				printf("NEW CONNECTION\n");
-				IP = inet_ntoa(carAddress.sin_addr);
-				
-				if (UDPsend_init(IP, all_d.seq_num) < 0) {
-					printf("ERROR IN UDP SEND INIT\n");
-					if (fp != NULL)
-						fclose(fp);
-					return 0;
+		
+		for(i = 0; i <= fdmax; i++){
+			if(FD_ISSET(i, &read_fds)){
+				switch(i) {
+					case KEYBOARD:
+						if (STARTED) {
+							STARTED = FALSE;
+							if (fp != NULL)
+								fclose(fp);
+							if (end())
+								return 0;
+							FD_CLR(sockt_UDP, &master);
+							fdmax = KEYBOARD;
+						}
+						else {
+							STARTED = TRUE;
+							clearBuffer();
+							FD_SET(sockt_UDP, &master);
+							if (fdmax < sockt_UDP)
+								fdmax = sockt_UDP;
+							if (CONN_CREATED)
+								createNewFile();
+						}
+						break;
+					default:
+						if (recvFromCar() < 0) {
+							if (fp != NULL)
+								fclose(fp);
+							return -1;
+						}
+						break;
 				}
-				
-				IP = getMyIP();
-				UDPsend(IP);
-				
-				time(&rawtime);
-				tm = localtime(&rawtime);
-				sprintf(filename, "./log/log_%04d-%02d-%02d_%02d-%02d-%02d.csv", tm->tm_year+1900, tm->tm_mon+1, tm->tm_mday, tm->tm_hour, tm->tm_min, tm->tm_sec);
-				if (fp != NULL)
-					fclose(fp);
-				fp = fopen(filename,"a");
-			}
-			else{
-				if (recvBytes == 16) {
-					printf("DATA\n");
-					fprintf(fp,"%d\t %f\t %f\t %f\t\n", all_d.seq_num, all_d.ax, all_d.ay, all_d.gz);
-				}
-				else
-					printf("ERROR\n");
 			}
 		}
+	}
 }
--- server/udp.c
+++ server/udp.c
@@ -9,11 +9,13 @@
 #include 	<string.h>
 #include 	<sys/ioctl.h>
 #include 	<sys/socket.h>
-
 #include 	"udp.h"
 
-#define PORT_TO_RECEIVE			8000
-#define NETWORK_INTERFACE 		"en0"
+//------------------------------------------------------------------------------
+// GLOBAL CONSTANTS
+//------------------------------------------------------------------------------
+
+#define 	NETWORK_INTERFACE 	"en0"
 
 //------------------------------------------------------------------------------
 // GLOBAL VARIABLES
@@ -21,7 +23,7 @@
 
 static address	myAddress, carAddress;	// Informations about peers
 
-static int 		sockRecv, sockSend;		// Sockets to send and receive
+static int 		socket_UDP;		// Sockets to send and receive
 
 //------------------------------------------------------------------------------
 // FUNCTIONS
@@ -31,98 +33,75 @@ static int 		sockRecv, sockSend;		// Sockets to send and receive
 // GET MY IP: Return the IP of the server
 //------------------------------------------------------------------------------
 
-char*  	getMyIP() {
-struct 	ifreq ifr;
+char* getMyIP() {
+	struct 	ifreq ifr;
 	
-		ifr.ifr_addr.sa_family = AF_INET;
-	
-		strncpy(ifr.ifr_name, NETWORK_INTERFACE, IFNAMSIZ-1);
-	
-		ioctl(sockRecv, SIOCGIFADDR, &ifr);
-	
-		return inet_ntoa(((address*) &ifr.ifr_addr)->sin_addr);
+	ifr.ifr_addr.sa_family = AF_INET;
+
+	strncpy(ifr.ifr_name, NETWORK_INTERFACE, IFNAMSIZ-1);
+
+	ioctl(socket_UDP, SIOCGIFADDR, &ifr);
+
+	return inet_ntoa(((address*) &ifr.ifr_addr)->sin_addr);
 }
 
 //------------------------------------------------------------------------------
-// UDP BIND: Initialization of the structure and execution of the bind operation
+// CREATE MY ADDRESS: Initialization of the structure myAddress
 //------------------------------------------------------------------------------
 
-static int 	UDPbind(void) {
-			// Initialization of the structure with board network's info
-			bzero((char *) &myAddress, sizeof(address));
-			myAddress.sin_len = (u_char) sizeof(address);
-			myAddress.sin_family = AF_INET;
-			myAddress.sin_port = htons(PORT_TO_RECEIVE);
-			myAddress.sin_addr.s_addr = htonl(INADDR_ANY);
-			
-			return bind(sockRecv, (sockaddr *) &myAddress, sizeof(address)); 
+void createMyAddress(int port) {
+	bzero((char *) &myAddress, sizeof(address));
+	myAddress.sin_len = (u_char) sizeof(address);
+	myAddress.sin_family = AF_INET;
+	myAddress.sin_port = htons(port);
+	myAddress.sin_addr.s_addr = htonl(INADDR_ANY);
 }
 
 //------------------------------------------------------------------------------
-// CREATE CAR ADDRESS: Creation of the structure of the car
+// CREATE CAR ADDRESS: Initialization of the structure carAddress
 //------------------------------------------------------------------------------
 
-static int 	createCarAddress(char * ip, int port) {
-			// Initialization of the struct with network's info of Unreal Engine
-			bzero((char *) &carAddress, sizeof(address));
-			carAddress.sin_len = (u_char) sizeof(address);
-			carAddress.sin_family = AF_INET;
-			carAddress.sin_port = htons(port);
-			carAddress.sin_addr.s_addr = inet_addr(ip);
-			return 0;
-}
-
-//------------------------------------------------------------------------------
-// UDP SEND INIT: Creation of the connection to send
-//------------------------------------------------------------------------------
-
-int 		UDPsend_init(char * ip, int port) {
-const int 	REUSE_ADDR = 1;			// Constant required to create a connection
-	
-			if ((sockSend = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
-				return -1;
-	
-			if (setsockopt(sockSend, SOL_SOCKET, SO_REUSEADDR, &REUSE_ADDR, sizeof(int)) < 0)
-				return -1;
-	
-			if (createCarAddress(ip, port) < 0)
-				return -1;
-	
-			return 0;
+void createCarAddress(char * ip, int port) {
+	bzero((char *) &carAddress, sizeof(address));
+	carAddress.sin_len = (u_char) sizeof(address);
+	carAddress.sin_family = AF_INET;
+	carAddress.sin_port = htons(port);
+	carAddress.sin_addr.s_addr = inet_addr(ip);
 }
 
 //------------------------------------------------------------------------------
 // UDP RECV INIT: Creation of the connection to receive
 //------------------------------------------------------------------------------
 
-int 		UDPrecv_init(void) {
-const int 	REUSE_ADDR = 1;			// Constant required to create a connection
+int UDPrecv_init(int port) {
+	const int REUSE_ADDR = 1;
 	
-			if ((sockRecv = socket(AF_INET, SOCK_DGRAM, 0)) < 0) 
-				return -1;
-			
-			if (setsockopt(sockRecv, SOL_SOCKET, SO_REUSEADDR, &REUSE_ADDR, sizeof(int)) < 0)
-				return -1;
-			
-			if (UDPbind() < 0)
-				return -1;
+	if ((socket_UDP = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+		return -1;
 	
-			return 0;
+	if (setsockopt(socket_UDP, SOL_SOCKET, SO_REUSEADDR, &REUSE_ADDR, sizeof(int)) < 0)
+		return -1;
+	
+	createMyAddress(port);
+	
+	if (bind(socket_UDP, (sockaddr *) &myAddress, sizeof(myAddress)) < 0)
+		return -1;
+		
+	return socket_UDP;
 }
 
 //------------------------------------------------------------------------------
 // UDP SEND: 	Send the server IP to the car
 //------------------------------------------------------------------------------
 
-void 	UDPsend(char * ip) {
-    	sendto(sockSend, ip, strlen(ip), 0, (sockaddr *) &carAddress, sizeof(address));
+ssize_t UDPsend(char * ip){
+	return sendto(socket_UDP, ip, strlen(ip), 0, (sockaddr *) &carAddress,sizeof(carAddress));
 }
 
 //------------------------------------------------------------------------------
 // UDP RECV: 	Receive sensor's data from the car
 //------------------------------------------------------------------------------
 
-int			UDPrecv(sensor_data_g_t * all_d, address * addr) {
-socklen_t 	sockAddrSize = sizeof(address);
-			return recvfrom(sockRecv, all_d, sizeof(sensor_data_g_t), MSG_WAITALL, (sockaddr *) addr, &sockAddrSize);
+ssize_t	UDPrecv(sensor_data_g_t * all_d) {
+	return recv(socket_UDP, all_d, sizeof(sensor_data_g_t), MSG_WAITALL);
 }
